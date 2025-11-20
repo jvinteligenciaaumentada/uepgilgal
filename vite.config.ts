f4f595417342696104cef2f616c0906fc9a25be0
jvinteligenciaaumentada <jv.inteligenciaaumentada@gmail.com>
2025-11-20 09:21:25 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-      base: '/uepgilgal/',
     const env = loadEnv(mode, '.', '');
     return {
+              base: '/uepgilgal/',
       server: {
         port: 3000,
         host: '0.0.0.0',
